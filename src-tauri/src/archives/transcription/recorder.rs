0d35a65181078925cc6d20d2476dbf902e6b41af
Alan Liu <liualan998@gmail.com>
2026-03-29 18:33:50 -0400

    [feature] recorder support for multiple data types

--- src-tauri/src/archives/transcription/recorder.rs
+++ src-tauri/src/archives/transcription/recorder.rs
@@ -2,15 +2,17 @@
 use std::sync::{Arc, Mutex};
 
 // external
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Device, Host, SampleFormat, Stream, SupportedInputConfigs, SupportedStreamConfig};
+use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::{
+    Device, FromSample, Host, Sample, SampleFormat, Stream, SupportedInputConfigs,
+    SupportedStreamConfig,
+};
 
 // internal
 use crate::archives::transcription::constants::TRANSCRIPTION_DESIRED_HZ;
 use crate::error::TranscriptionError;
 
 pub struct AudioRecorder {
-    host: Host,
     device: Device,
 }
 
@@ -23,7 +25,7 @@ impl AudioRecorder {
             None => return Err(TranscriptionError::NoDevicesFound),
         };
 
-        Ok(AudioRecorder { host, device })
+        Ok(AudioRecorder { device })
     }
 
     pub fn build_input_stream(
@@ -39,43 +41,41 @@ impl AudioRecorder {
 
         let callback_buffer_ref: Arc<Mutex<Vec<f32>>> = Arc::clone(audio_buffer);
 
-        let channels = supported_config.config().channels as usize;
+        let channels: usize = supported_config.config().channels.into();
+        let sample_format = supported_config.sample_format();
 
-        self.device
-            .build_input_stream(
+        match sample_format {
+            SampleFormat::F32 => self.device.build_input_stream(
                 &supported_config.config(),
                 move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                    if channels == 1 {
-                        if !data.is_empty() {
-                            let mut local = Vec::with_capacity(data.len());
-                            local.extend_from_slice(data);
-                            if let Ok(mut buffer) = callback_buffer_ref.lock() {
-                                buffer.extend_from_slice(&local);
-                            }
-                        }
-                    } else if channels > 1 {
-                        let frames = data.len() / channels;
-                        if frames == 0 {
-                            return;
-                        }
-                        let mut mono: Vec<f32> = Vec::with_capacity(frames);
-                        for frame_idx in 0..frames {
-                            let mut sum = 0.0f32;
-                            let base = frame_idx * channels;
-                            for channel in 0..channels {
-                                sum += data[base + channel];
-                            }
-                            mono.push(sum / (channels as f32));
-                        }
-                        if let Ok(mut buffer) = callback_buffer_ref.lock() {
-                            buffer.extend_from_slice(&mono);
-                        }
-                    }
+                    process_and_append(data, channels, &callback_buffer_ref)
                 },
                 move |err| println!("{:?}", err),
                 None,
-            )
-            .map_err(|err| TranscriptionError::InternalError(err.to_string()))
+            ),
+            SampleFormat::I16 => self.device.build_input_stream(
+                &supported_config.config(),
+                move |data: &[i16], _: &cpal::InputCallbackInfo| {
+                    process_and_append(data, channels, &callback_buffer_ref)
+                },
+                move |err| println!("{:?}", err),
+                None,
+            ),
+            SampleFormat::U16 => self.device.build_input_stream(
+                &supported_config.config(),
+                move |data: &[u16], _: &cpal::InputCallbackInfo| {
+                    process_and_append(data, channels, &callback_buffer_ref)
+                },
+                move |err| println!("{:?}", err),
+                None,
+            ),
+            _ => {
+                return Err(TranscriptionError::InternalError(
+                    "Unsupported sample format".to_string(),
+                ))
+            }
+        }
+        .map_err(|err| TranscriptionError::InternalError(err.to_string()))
     }
 }
 
@@ -124,4 +124,38 @@ fn choose_input_config(
     }
 
     Err(TranscriptionError::UnsupportedSampleRange(target_hz))
+}
+
+fn process_and_append<T: Sample>(data: &[T], channels: usize, cb: &Arc<Mutex<Vec<f32>>>)
+where
+    f32: FromSample<T>,
+{
+    if channels == 1 {
+        if !data.is_empty() {
+            let mut tmp: Vec<f32> = Vec::with_capacity(data.len());
+            for &s in data.iter() {
+                tmp.push(f32::from_sample(s));
+            }
+            if let Ok(mut buffer) = cb.lock() {
+                buffer.extend_from_slice(&tmp);
+            }
+        }
+    } else if channels > 1 {
+        let frames = data.len() / channels;
+        if frames == 0 {
+            return;
+        }
+        let mut mono: Vec<f32> = Vec::with_capacity(frames);
+        for frame_idx in 0..frames {
+            let base = frame_idx * channels;
+            let mut sum = 0.0f32;
+            for ch in 0..channels {
+                sum += f32::from_sample(data[base + ch]);
+            }
+            mono.push(sum / (channels as f32));
+        }
+        if let Ok(mut buffer) = cb.lock() {
+            buffer.extend_from_slice(&mono);
+        }
+    }
 }
