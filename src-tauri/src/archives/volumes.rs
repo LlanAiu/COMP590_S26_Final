c219323c33c82ed16571f91085f2336173ea52c9
Alan Liu <liualan998@gmail.com>
2026-04-03 13:28:20 -0400

    [setup] basic volume structure

--- src-tauri/src/archives/volumes.rs
+++ src-tauri/src/archives/volumes.rs
@@ -1,17 +1,21 @@
 // builtin
+use std::future::Future;
 
 // external
 
 // internal
 
 // modules
+pub mod implementations;
+pub mod subsystems;
+pub mod types;
 
 pub trait VolumeDatabase {
-    fn create_volume();
+    fn create_volume(&self) -> impl Future<Output = ()> + Send;
 
-    fn read_volume();
+    fn read_volume(&self) -> impl Future<Output = ()> + Send;
 
-    fn edit_volume();
+    fn edit_volume(&self) -> impl Future<Output = ()> + Send;
 
-    fn delete_volume();
+    fn delete_volume(&self) -> impl Future<Output = ()> + Send;
 }
